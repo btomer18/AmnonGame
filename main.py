48166b16ba8591bbce1ab96b1c3ed88199ed5b5f
Tomer Bar <btomer18@gmail.com>
2020-12-24 12:35:53 +0200

    changed colors to sea + board analyze completed (untill num=3)

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+from typing import List, Tuple
+
 from PIL import Image
 import pyautogui
 
@@ -5,12 +8,49 @@ BOARD_LENGTH = 9
 BOARD_HEIGHT = 9
 
 BOARD_WALL_RGB = (128, 128, 128)
-BOARD_POSITION = (1411, 274, 1626, 489)
-BOX_SIZE_PX = 24
+BOARD_POSITION = (1399, 383, 1612, 596)
+BOX_SIZE = 21
+BOX_DELIMITER_SIZE = 2
+
+BLUE = (0, 0, 255)
+GREEN = (0, 128, 0)
+RED = (253, 4, 6)
+EXPLODE_COLOR = (245, 137, 138)
+BOMB_COLOR = (209, 209, 209)
+DISCOVERED_COLOR = (215, 212, 255)
+UNDISCOVERED_COLORS = [
+    (0, 103, 96),
+    (4, 183, 173),
+]
+
+BOXES_COLORS = OrderedDict(
+    [
+        ("1", [BLUE, DISCOVERED_COLOR]),
+        ("2", [GREEN, DISCOVERED_COLOR]),
+        ("3", [RED, DISCOVERED_COLOR]),
+        ("bomb_guess", [BOMB_COLOR] + UNDISCOVERED_COLORS),
+        ("exploded_bomb", [EXPLODE_COLOR, BOMB_COLOR]),
+        ("undiscovered", UNDISCOVERED_COLORS),
+    ]
+)
 
 
 def main():
     board = capture_board()
+    board = analyze_board(board)
+
+
+def analyze_board(board: List[List[Image.Image]]) -> List[List[Tuple[Image.Image, str]]]:
+    return [[analyze_box(box) for box in row] for row in board]
+
+
+def analyze_box(box: Image.Image) -> Tuple[Image.Image, str]:
+    pixels = list(box.getdata())
+    for box_type in BOXES_COLORS.keys():
+        if all(color in pixels for color in BOXES_COLORS[box_type]):
+            return box, box_type
+
+    raise UnrecognizedBoxException()
 
 
 def capture_board():
@@ -19,12 +59,16 @@ def capture_board():
 
     return [
         [
-            board.crop((x, y, x + BOX_SIZE_PX, y + BOX_SIZE_PX))
-            for x in range(0, board.size[0], BOX_SIZE_PX)
+            board.crop((x, y, x + BOX_SIZE, y + BOX_SIZE))
+            for x in range(0, board.size[0], BOX_SIZE + BOX_DELIMITER_SIZE + 1)
         ]
-        for y in range(0, board.size[1], BOX_SIZE_PX)
+        for y in range(0, board.size[1], BOX_SIZE + BOX_DELIMITER_SIZE + 1)
     ]
 
 
 if __name__ == "__main__":
     main()
+
+
+class UnrecognizedBoxException(Exception):
+    pass
